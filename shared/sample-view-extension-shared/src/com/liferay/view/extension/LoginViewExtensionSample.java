3a7ad48eddac7122d15980d941b6bfaf4d87d1f6
Carlos Sierra <carlos.sierra@liferay.com>
2014-08-08 10:17:34 +0200

    WIP

--- shared/sample-view-extension-shared/src/com/liferay/view/extension/LoginViewExtensionSample.java
+++ shared/sample-view-extension-shared/src/com/liferay/view/extension/LoginViewExtensionSample.java
@@ -15,20 +15,33 @@
 package com.liferay.view.extension;
 
 import com.liferay.taglib.util.PortletViewExtension;
+import org.apache.jasper.servlet.JspServlet;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
 
 import javax.portlet.PortletURL;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 import javax.portlet.WindowState;
 import javax.portlet.WindowStateException;
+import javax.servlet.Servlet;
 import java.io.IOException;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 /**
  * @author Carlos Sierra Andrés
  */
 @Component(property = {"extension-id=login-form-after-password"})
 public class LoginViewExtensionSample implements PortletViewExtension {
+
+	private ServiceRegistration<Servlet> _servletServiceRegistration;
 
 	@Override
 	public void render(
@@ -45,7 +58,37 @@ public class LoginViewExtensionSample implements PortletViewExtension {
 
 		renderResponse.getWriter().write("<a href=\""+actionURL.toString()+"\">extension link</a>");
 
+		renderRequest.
+			getPortletSession().
+			getPortletContext().
+			getRequestDispatcher("/o/sample-view-extension/extension.jsp").
+	}
 
+	@Activate
+	protected void createJspServlet() {
+
+		Dictionary<String, Object> properties = new Hashtable<String, Object>();
+
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_SELECT,
+			"sample-view-extension");
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_NAME, "jsp");
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_PATTERN, "*.jsp");
+		properties.put(
+			"servlet.init.compilerClassName",
+			"com.liferay.portal.servlet.jsp.compiler.compiler.JspCompiler");
+		properties.put("servlet.init.httpMethods", "GET,POST,HEAD");
+		properties.put("servlet.init.keepgenerated", "true");
+		properties.put("servlet.init.logVerbosityLevel", "DEBUG");
+
+		Bundle bundle = FrameworkUtil.getBundle(LoginViewExtensionSample.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		_servletServiceRegistration = bundleContext.registerService(
+			Servlet.class, new JspServlet(), properties);
 	}
 
 }
