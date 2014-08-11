80d79d4fbec4590f823eab1e998654f9a1c3b81c
Carlos Sierra <carlos.sierra@liferay.com>
2014-08-11 16:28:56 +0200

    LPS-49252 Register a JSP servlet to offload extension rendering

--- shared/sample-view-extension-shared/src/com/liferay/view/extension/LoginViewExtensionSample.java
+++ shared/sample-view-extension-shared/src/com/liferay/view/extension/LoginViewExtensionSample.java
@@ -15,17 +15,35 @@
 package com.liferay.view.extension;
 
 import com.liferay.kernel.servlet.taglib.ViewExtension;
+import com.liferay.portal.kernel.util.JavaConstants;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 import javax.portlet.PortletURL;
 import javax.portlet.RenderResponse;
 import javax.portlet.WindowState;
 import javax.portlet.WindowStateException;
 
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.jasper.servlet.JspServlet;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.http.context.ServletContextHelper;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
 
 /**
  * @author Carlos Sierra Andrés
@@ -33,7 +51,60 @@ import javax.servlet.http.HttpServletResponse;
 @Component(
 	immediate = true, property = {"extension-id=login-form-after-password"},
 	service = ViewExtension.class)
-public class LoginViewExtensionSample implements ViewExtension {
+public class LoginViewExtensionSample implements ViewExtension{
+
+	@Activate
+	protected void createJspServlet() {
+		Bundle bundle = FrameworkUtil.getBundle(LoginViewExtensionSample.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		Dictionary<String, Object> properties = new Hashtable<String, Object>();
+
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_NAME,
+			"sample-view-extension");
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_PATH,
+			"/sample-view-extension");
+
+		ServletContextHelper servletContextHelper = new ServletContextHelper(
+			bundle) {};
+
+		_contextServiceRegistration = bundleContext.registerService(
+			ServletContextHelper.class, servletContextHelper, properties);
+
+		properties = new Hashtable<String, Object>();
+
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_SELECT,
+			"sample-view-extension");
+
+		properties = new Hashtable<String, Object>();
+
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_SELECT,
+			"sample-view-extension");
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_NAME, "jsp");
+		properties.put(
+			HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_PATTERN, "*.jsp");
+		properties.put(
+			"servlet.init.compilerClassName",
+			"com.liferay.portal.servlet.jsp.compiler.compiler.JspCompiler");
+		properties.put("servlet.init.httpMethods", "GET,POST,HEAD");
+		properties.put("servlet.init.keepgenerated", "true");
+		properties.put("servlet.init.logVerbosityLevel", "DEBUG");
+
+		_servletServiceRegistration = bundleContext.registerService(
+			Servlet.class, new JspServlet(), properties);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_servletServiceRegistration.unregister();
+		_contextServiceRegistration.unregister();
+	}
 
 	@Override
 	public void render(HttpServletRequest request, HttpServletResponse response)
@@ -57,5 +128,9 @@ public class LoginViewExtensionSample implements ViewExtension {
 		writer.write(
 			"<a href=\"" + actionURL.toString() + "\">extension link</a>");
 	}
+
+	private ServiceRegistration<Servlet> _servletServiceRegistration;
+	private ServiceRegistration<ServletContextHelper>
+		_contextServiceRegistration;
 
 }
